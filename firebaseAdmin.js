bc083cb9446748a948636401a38328718ecdc7a2
Marilou Ranada <ailouranada@gmail.com>
2025-01-08 00:44:33 +0800

    another try

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,7 +2,7 @@ import admin from 'firebase-admin';
 
 // Check if the admin SDK is already initialized
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse('serviceAccountKey.json'); // Or import your serviceAccountKey.json
+  // const serviceAccount = JSON.parse('serviceAccountKey.json'); // Or import your serviceAccountKey.json
   admin.initializeApp({
     credential: admin.credential.cert('serviceAccountKey.json'),
   });
